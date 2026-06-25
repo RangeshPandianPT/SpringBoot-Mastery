9faddde98bf61a0a8c8a73681f484d183a3b6a51
RangeshPandianPT <rangeshpandian@gmail.com>
2026-06-25 09:54:48 +0530

    config: Register RequestLoggingInterceptor in WebConfig

---
+++ HelloWorld/src/main/java/dev/rangesh/HelloWorld/config/WebConfig.java
@@ -0,0 +1,24 @@
+package dev.rangesh.HelloWorld.config;
+
+import dev.rangesh.HelloWorld.interceptor.RequestLoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final RequestLoggingInterceptor requestLoggingInterceptor;
+
+    @Autowired
+    public WebConfig(RequestLoggingInterceptor requestLoggingInterceptor) {
+        this.requestLoggingInterceptor = requestLoggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // Register the interceptor for all API endpoints
+        registry.addInterceptor(requestLoggingInterceptor).addPathPatterns("/api/**");
+    }
+}

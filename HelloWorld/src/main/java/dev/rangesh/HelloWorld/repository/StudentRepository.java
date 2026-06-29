1d9e4e961977b17cb4ac487d03b169da3e74590d
RangeshPandianPT <rangeshpandian@gmail.com>
2026-06-29 18:53:56 +0530

    feat: create Spring Data JPA StudentRepository

---
+++ HelloWorld/src/main/java/dev/rangesh/HelloWorld/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package dev.rangesh.HelloWorld.repository;
+
+import dev.rangesh.HelloWorld.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
